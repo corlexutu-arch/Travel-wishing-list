9249b5923c136be44e7d40014d464a1518706eaa
corlexutu-arch <corlexutu@gmail.com>
2026-08-19 19:27:07 +0300

    Fix missing get_object_or_404 import

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from mainapp.models import DestinatieVacanta
 from django.contrib import messages
 from django.db.models import Q
